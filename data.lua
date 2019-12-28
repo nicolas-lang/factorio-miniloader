10f5b1d0ea154c18a00de8f31ea8be610c049757
mspielberg <9729801+mspielberg@users.noreply.github.com>
2019-12-27 18:04:33 -0800

    add settings to disable/enable classes of loaders

--- data.lua
+++ data.lua
@@ -13,22 +13,26 @@ local function create_miniloader(prefix, next_prefix, tech_prereqs, tint, base_u
   technologies.create_technology(prefix, tech_prereqs, tint)
 end
 
-for prefix, args in pairs(templates) do
-  create_miniloader(prefix, args.next_prefix, args.prerequisite_techs, args.tint, args.base_underground_name)
+if settings.startup["miniloader-enable-standard"].value then
+  for prefix, args in pairs(templates) do
+    create_miniloader(prefix, args.next_prefix, args.prerequisite_techs, args.tint, args.base_underground_name)
+  end
 end
 
 -- chute
-create_miniloader("chute-", "", {"logistics"}, {r=0.5,g=0.5,b=0.5}, "underground-belt")
+if settings.startup["miniloader-enable-chute"].value then
+  create_miniloader("chute-", "", {"logistics"}, {r=0.5,g=0.5,b=0.5}, "underground-belt")
 
-data.raw.technology["chute-miniloader"] = nil
-data.raw.recipe["chute-miniloader"].enabled = true
-data.raw.loader["chute-miniloader-loader"].speed = data.raw.loader["chute-miniloader-loader"].speed / 4
-data.raw.inserter["chute-miniloader-inserter"].rotation_speed = data.raw.inserter["chute-miniloader-inserter"].rotation_speed / 4
-data.raw.inserter["chute-miniloader-inserter"].energy_source = {type="void"}
-data.raw.inserter["chute-miniloader-inserter"].energy_per_movement = ".0000001J"
-data.raw.inserter["chute-miniloader-inserter"].energy_per_rotation = ".0000001J"
+  data.raw.technology["chute-miniloader"] = nil
+  data.raw.recipe["chute-miniloader"].enabled = true
+  data.raw.loader["chute-miniloader-loader"].speed = data.raw.loader["chute-miniloader-loader"].speed / 4
+  data.raw.inserter["chute-miniloader-inserter"].rotation_speed = data.raw.inserter["chute-miniloader-inserter"].rotation_speed / 4
+  data.raw.inserter["chute-miniloader-inserter"].energy_source = {type="void"}
+  data.raw.inserter["chute-miniloader-inserter"].energy_per_movement = ".0000001J"
+  data.raw.inserter["chute-miniloader-inserter"].energy_per_rotation = ".0000001J"
 
-data.raw.item["chute-filter-miniloader"] = nil
-data.raw.recipe["chute-filter-miniloader"] = nil
-data.raw.loader["chute-filter-miniloader-loader"] = nil
-data.raw.inserter["chute-filter-miniloader-inserter"] = nil
+  data.raw.item["chute-filter-miniloader"] = nil
+  data.raw.recipe["chute-filter-miniloader"] = nil
+  data.raw.loader["chute-filter-miniloader-loader"] = nil
+  data.raw.inserter["chute-filter-miniloader-inserter"] = nil
+end
